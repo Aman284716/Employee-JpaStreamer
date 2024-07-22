7fdcb557993b02ed17b3f734f45271a639c97d8b
Aman284716 <284716@ust.com>
2024-07-22 10:29:14 +0530

    initial Commit

---
+++ Employee-JpaStreamer/src/main/java/com/ust/Employee_JpaStreamer/EmployeeJpaStreamerApplication.java
@@ -0,0 +1,13 @@
+package com.ust.Employee_JpaStreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeJpaStreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeJpaStreamerApplication.class, args);
+	}
+
+}
